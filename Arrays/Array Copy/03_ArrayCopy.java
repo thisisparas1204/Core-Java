975deb1c80d94a235fa28b9ae7ef5b00cea3c403
Paras Bishwas <116245879+thisisparas1204@users.noreply.github.com>
2023-04-04 01:27:11 -0400

    Update and rename ArrayCopy3.java to 03_ArrayCopy.java

--- Arrays/Array Copy/03_ArrayCopy.java
+++ Arrays/Array Copy/03_ArrayCopy.java
@@ -1,7 +1,7 @@
 // Copying Arrays Using System.arraycopy() 
 import java.util.Arrays;
 
-public class ArrayCopy3 {
+public class ArrayCopy {
     public static void main(String[] args) {
         
         int[] data = new int[] {10, 20, 30};
