c57b692ba8c1cb3f0a1a7942fb80f74551e895da
Paras Bishwas <116245879+thisisparas1204@users.noreply.github.com>
2023-06-16 02:17:26 -0400

    Update 02_JaggedArrayInput.java
    Optimized this code.

--- Arrays/Jagged Arrays/02_JaggedArrayInput.java
+++ Arrays/Jagged Arrays/02_JaggedArrayInput.java
@@ -13,30 +13,13 @@ public class JaggedArrayInput {
         data[1] = new int[2];
         data[2] = new int[4];
 
-        System.out.println("Enter the elements for 1st member array..");
-
-        for (int member_array = 0; member_array < 1; ++member_array) {
-            for (int j = 0; j < data[0].length; ++j) {
-                data[member_array][j] = input.nextInt();
+        for (int i = 0; i < data.length; ++i) {
+            System.out.println("Enter the elements for " + (i + 1) + " member array.");
+            for (int j = 0; j < data[i].length; ++j) {
+                data[i][j] = input.nextInt();
             }
         }
-
-        System.out.println("Enter the elements for 2nd member array..");
-
-        for (int member_array = 1; member_array < 2; ++member_array) {
-            for (int j = 0; j < data[1].length; ++j) {
-                data[member_array][j] = input.nextInt();
-            }
-        }
-
-        System.out.println("Enter the elements for 3rd member array..");
-
-        for (int member_array = 2; member_array < 3; ++member_array) {
-            for (int j = 0; j < data[2].length; ++j) {
-                data[member_array][j] = input.nextInt();
-            }
-        }
-
+        
         input.close();
 
         System.out.println("The array elements are: " + Arrays.deepToString(data));
